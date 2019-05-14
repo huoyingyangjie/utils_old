f9cfe77d696057e0173e81c36ff10436baf12927
root <root@localhost.localdomain>
2019-05-14 10:18:30 +0800

    update

---
+++ src/test/version/main.c
@@ -0,0 +1,13 @@
+//
+// Created by yangjiegogogo@gmail.com on 5/13/19.
+//
+
+
+#include "utils/debug.h"
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+    printf("Version:%s\n",strfversion());
+
+}
